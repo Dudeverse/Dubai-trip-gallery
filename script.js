6f1120a8c68fb327adeb970e63c60f35b596ded5
Dudeverse <se352@njit.edu>
2023-08-07 21:32:04 +0530

    added buttons, made images polaroids

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-// Add images here
-const imageUrls = [
-    "images/ALSEEF1.jpeg",
-    "images/MALL2.jpeg",
-    "images/ALSEEF2.jpeg",
-    "images/MALL1.jpeg",
-    "images/LOCALSHOP.jpeg",
-    "images/ALSEEF3.jpeg",
-    "images/DUBAIMALL3.jpeg",
-    "images/WATER_TAXI_2.jpeg",
-    "images/ALFAHIDI.jpeg",
-    "images/BURJ1.jpeg",
-    "images/KUNAFA.jpeg",
-    "images/NABLUS.jpeg",
-];
-
-// function for adding images dynamically
-function createGalleryImages() {
-    const gallery = document.querySelector(".gallery");
-    imageUrls.forEach((imageUrl) => {
-    const img = document.createElement("img");
-    img.src = imageUrl;
-    img.alt = "Travel Image";
-    img.addEventListener("mouseover", () => toggleImageAnimation(img));
-    gallery.appendChild(img);
-    });
-}
-
-//function for animation effect
-function toggleImageAnimation(img) {
-    if (!img.classList.contains("animate-image")) {
-    img.classList.add("animate-image");
-    img.addEventListener("animationend", () => {
-        img.classList.remove("animate-image");
-      }, { once: true }); 
+// function for animation effect
+function toggleDivAnimation(img) {
+    if (!img.classList.contains("animate-div")) {
+        img.classList.add("animate-div");
+        img.addEventListener("animationend", () => {
+            img.classList.remove("animate-div");
+        }, { once: true });
     }
 }
 
 // gallery appears after page load
-window.addEventListener("load", createGalleryImages);
+window.addEventListener("load", () => {
+    // Select all the divs with the class "polaroid"
+    const polaroidDivs = document.querySelectorAll(".polaroid");
+
+    // Attach the animation event listener to each "polaroid" div
+    polaroidDivs.forEach((img) => {
+        img.addEventListener("mouseover", () => toggleDivAnimation(img));
+    });
+});
