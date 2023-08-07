66dbcc53646268c74acb08ba99c9fa7b2ea793d0
Dudeverse <se352@njit.edu>
2023-08-07 21:48:14 +0530

    done for today

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Dubai-trip-gallery
 
-An attempt to build a responsive image gallery, using css Flexbox and grid.
+An attempt to build a responsive image gallery, using css Flexbox.
+
+Topics practiced and learnt:
+
+1. CSS Flexbox
+
+2. Javascript functions
+
+3. CSS 2d transforms and animations
+
+
 
--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function toggleDivAnimation(img) {
 
 // gallery appears after page load
 window.addEventListener("load", () => {
-    // Select all the divs with the class "polaroid"
+    // Select all the images with the class "polaroid"
     const polaroidDivs = document.querySelectorAll(".polaroid");
 
-    // Attach the animation event listener to each "polaroid" div
+    // Attach the animation event listener to each "polaroid" img
     polaroidDivs.forEach((img) => {
         img.addEventListener("mouseover", () => toggleDivAnimation(img));
     });
